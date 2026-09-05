bcc80678129687be0b3566a91c0fb5209b69a378
Anshu Kumar <anshuks001@gmail.com>
2026-09-05 23:13:51 +0530

    Time: 4 ms (88.55%) | Memory: 133.2 MB (20.99%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int[] suffMin = new int[n];
+        suffMin[n - 1] = nums[n - 1];
+        
+        for (int i = n - 2; i >= 0; i--) {
+            suffMin[i] = Math.min(suffMin[i + 1], nums[i]);
+        }
+        
+        int curMax = nums[0];
+        for (int i = 0; i < n; i++) {
+            curMax = Math.max(curMax, nums[i]);
+            if (curMax - suffMin[i] <= k) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
